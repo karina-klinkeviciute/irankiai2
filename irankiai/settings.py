35fbea71c0669645febda0dc44e0bdf93fba6b80
karina <karina.klinkeviciute@gmail.com>
2024-06-14 15:23:29 +0300

    allowed hosts

--- irankiai/settings.py
+++ irankiai/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['165.22.87.133', "127.0.0.1", "localhost"]
 
 
 # Application definition
